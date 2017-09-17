bc1b28f596e2682fa98f32b9a2a9f6e2d8bd1691
Samaritan89 <sunluyong@gmail.com>
2017-09-17 21:23:57 +0800

    feat: 覆盖率

--- src/math.js
+++ src/math.js
@@ -1,3 +1,8 @@
+function min(a, b) {
+  const c = 3;
+  return (b - a) * c;
+}
+
 module.exports = {
   add: (...args) => {
     return args.reduce((prev, curr) => {
@@ -9,5 +14,15 @@ module.exports = {
     return args.reduce((prev, curr) => {
       return prev * curr;
     });
+  },
+
+  cover: (a, b) => {
+    if (a > b) {
+      return a - b;
+    } else if (a == b) {
+      return a + b;
+    } else {
+      return min(a, b);
+    }
   }
 };
--- test/mocha.js
+++ test/mocha.js
@@ -1,5 +1,5 @@
 const {should, expect, assert} = require('chai');
-const {add, mul} = require('../src/math');
+const {add, mul, cover} = require('../src/math');
 
 describe('#math', () => {
   describe('add', () => {
@@ -7,7 +7,7 @@ describe('#math', () => {
       expect(add(2, 3), 5);
     });
 
-    it.skip('should return 5 when 2 + 3', () => {
+    it('should return 5 when 2 + 3', () => {
       expect(add(2, -3), -1);
     });
   });
@@ -15,6 +15,20 @@ describe('#math', () => {
   describe('mul', () => {
     it('should return 6 when 2 * 3', () => {
       expect(mul(2, 3), 6);
+    });
+  });
+
+  describe('cover', () => {
+    it('should return 1 when cover(2, 1)', () => {
+      expect(cover(2, 1)).to.equal(1);
+    });
+
+    it('should return 3 when cover(1, 2)', () => {
+      expect(cover(1, 2)).to.equal(3);
+    });
+
+    it('should return 4 when cover(2, 2)', () => {
+      expect(cover(2, 2)).to.equal(4);
     });
   });
 });
