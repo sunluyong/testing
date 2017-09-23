3bb39981dcc9bbb377b8a3c5c5fc9837dbea9cb8
Samaritan89 <sunluyong@gmail.com>
2017-09-23 16:23:10 +0800

    fix: UI 组件 src

---
+++ src/demo.js
@@ -0,0 +1,50 @@
+import React, {PureComponent} from 'react';
+
+class Demo extends PureComponent {
+  static defaultProps = {
+    title: 'This is a demo',
+    value: 0
+  };
+
+  constructor (props) {
+    super(props);
+    const { title, value } = props;
+    this.state = {
+      title,
+      value
+    };
+  }
+
+  add = () => {
+    this.setState({
+      value: this.state.value ++
+    });
+  }
+
+  change = ev => {
+    this.setState({
+      value: ev.target.value
+    });
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      title: nextProps.title
+    });
+  }
+
+  render (){
+    return (
+      <div className="container">
+        <h1>{this.state.title}</h1>
+        <div className="counter">{this.state.value}</div>
+        <input
+          value={this.state.value}
+          onChange={this.change} />
+        <button onClick={this.add}>Value ++</button>
+      </div>
+    );
+  }
+}
+
+export default Demo;
